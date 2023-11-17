0994671310cc8219227d559bee8fed4ebc1b0e9a
juyeong-repo <juyeong.park.tech@gmail.com>
2023-11-17 14:02:24 +0900

    Test : console 찍기, index.html 테스트

---
+++ vanilla/Basic/main.js
@@ -0,0 +1,20 @@
+
+
+/*
+ 2023.11.17 
+SPA : 하나의 페이지에서 정보를 받아와 필요한 부분만 업데이트
+리액트 , 앵귤러, 뷰 등을 통해 spa를 쉽게 구현할 수 있음
+
+강력한 js 엔진을 정책한 애 : Node.js 
+
+This file:: 
+Hello world
+preparation
+async defer 
+
+​*/
+
+console.log('Hello, world!');
+
+
+// html과 연결
