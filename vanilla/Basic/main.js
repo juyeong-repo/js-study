f71d67fc1c1617e242028a8b45f0e4305a50c328
juyeong-repo <juyeong.park.tech@gmail.com>
2023-11-17 14:14:26 +0900

    Feat : async vs defer , use strict

--- vanilla/Basic/main.js
+++ vanilla/Basic/main.js
@@ -1,5 +1,10 @@
 
 
+
+// 자바스크립트 쓸 때는 유즈 스트릭 추천. ts 쓸 때는 얘 쓸 필요없다! js는 빨리 만드느라 유연하게 만들어졌음. 그만큼 위험요소가 있어서~ ecma5에서 추가된 기능
+'use strict';
+
+
 /*
  2023.11.17 
 SPA : 하나의 페이지에서 정보를 받아와 필요한 부분만 업데이트
@@ -18,3 +23,5 @@ console.log('Hello, world!');
 
 
 // html과 연결
+// 자바스크립트 공식사이트: ecma 
+//보통 프론트엔드 개발자가 방문하는 곳(mdn) : https://developer.mozilla.org/ko/docs/Web/JavaScript  (html,css 공부하기에도 좋음)
