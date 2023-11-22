a4464d14ad31ad266092fb21db07649816c6fb61
juyeong-repo <juyeong.park.tech@gmail.com>
2023-11-22 10:58:52 +0900

    Feat : ESM module system

--- react/section1/index.js
+++ react/section1/index.js
@@ -1,10 +1,9 @@
-const moduledata = require("./math");
+import mul, { add, sub } from "./math.js"
+// import mul from "./math.js"
 
-// const {add, sub} = require("./math");
-
-
-console.log(moduledata.add(1,2)); //3
-console.log(moduledata.sub(1,2)); //3
+console.log(add(1,2)); //3
+console.log(sub(1,2)); //3
+console.log(mul(10,20)); //200
 
 
 // 비슷한 역할을 하는 함수들은 하나의 파일에 보관하자 -> math.js
--- react/section1/math.js
+++ react/section1/math.js
@@ -1,16 +1,17 @@
-function add(a,b) {
+export function add(a,b) {
     return a+b;
 }
 
-function sub(a,b) {
+export function sub(a,b) {
     return a-b;
 } 
 
 // 클래스간 객체를 내보내고, 들여오는 것 -> 모듈 시스템. (old : CJS, new: ESM 둘 다 알아야 함)
-// CJS :  export , require
-module.exports = {
-    add,
-    sub,
-};
+// ESM :
 
+// export { add, sub };
 
+// 기본값으로 내보내기 -> math 모듈을 대표하는 값
+export default function multiply(a,b) {
+    return a * b;
+}
