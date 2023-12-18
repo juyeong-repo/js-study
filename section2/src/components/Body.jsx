8f1bbdf9a25ab99433f9f19bb7224955c627e1d0
juyeong-repo <juyeong.park.tech@gmail.com>
2023-12-18 20:41:11 +0900

    Feat : React Props, Counter App 초기 세팅

--- section2/src/components/Body.jsx
+++ section2/src/components/Body.jsx
@@ -2,13 +2,28 @@ import "./Body.css";
 import Button from "./button";
 
 export default function Body() {
+
+    const buttonProps = {
+        text : "1st button",
+        color : "Green",
+        a: 1,
+        b: 2,
+        c: 3,
+    };
+
+
     return (
 
         <div className="body">
             <h1>body</h1>
-            <Button />
-            <Button />
-            <Button />
+            <Button {...buttonProps}>
+                <div> button </div>
+            </Button>
+            <Button text= {"2"}/>
+            <Button text= {"3"} /> 
         </div>
         );
 }
+
+// 객체로 묶여온 props, IF 여러개의 props를 전달한다면? 
+// 컴포넌트에게 children 객체로 전달하려면? 
--- section2/src/components/button.jsx
+++ section2/src/components/button.jsx
@@ -1,5 +1,18 @@
 import "./Button.css";
 
-export default function Button() {
-    return <botton className = "button"> 클릭 </botton>;
+
+export default function Button({color, text, children}) {
+    console.log("-------" + props);
+
+    const {color, text} = props;
+    return <button 
+        style = {{
+            backgroudColor : color,
+        }}
+        className = "button"> 
+        
+        {children}
+         </button>;
 }
+
+
